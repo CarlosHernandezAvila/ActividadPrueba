ef9f1313e5d4d32fc2ff5dd1e31e5a84188c68cf
CarlosHernandezAvila <36894073+CarlosHernandezAvila@users.noreply.github.com>
2018-03-09 00:32:21 -0400

    Se sube proyecto de conexion con la BD Cmmoneycash atravez de web services

---
+++ WebServ_CarlosHernandez/WcfServ_CarlosHernandez/IServiceDataBase.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServ_CarlosHernandez
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IServ_Cmmoneycash
+    {
+
+
+        [OperationContract]
+        Correspon ObtenerCorrespon(string Cor_Nombre, int Cor_ID);
+        [OperationContract]
+        Oficinas ObtenerOficinas(string Ofi_nombre, int Ofi_ID);
+
+        // TODO: agregue aquí sus operaciones de servicio
+    }
+
+
+    [DataContract]
+    public class Correspon : CorresponRespuesta
+
+    {
+        [DataMember]
+        public string Cor_nombre { get; set; }
+        [DataMember]
+        public int Cor_ID { get; set; }
+
+    }
+
+    [DataContract]
+    public class CorresponRespuesta
+    {
+        public string RespuestaCorrespon { get; set; }
+        public string ErrorCorrespon { get; set; }
+    }
+
+
+    [DataContract]
+
+    public class Oficinas : OficinaRespuesta
+    {
+        [DataMember]
+        public string Ofi_nombre { get; set; }
+        [DataMember]
+        public int Ofi_ID { get; set; }
+
+    }
+
+    [DataContract]
+
+    public class OficinaRespuesta
+    {
+        public string RespuestaOficina { get; set; }
+        public string ErrorOficina { get; set; }
+    }
+
+}
---
+++ WebServ_CarlosHernandez/WcfServ_CarlosHernandez/ServiceDataBase.svc.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServ_CarlosHernandez
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Serv_Cmmomneycash : IServ_Cmmoneycash
+    {
+
+
+        //public string GetData(int value)
+        //{
+        //    return string.Format("You entered: {0}", value);
+        //}
+
+        //public CompositeType GetDataUsingDataContract(CompositeType composite)
+        //{
+        //    if (composite == null)
+        //    {
+        //        throw new ArgumentNullException("composite");
+        //    }
+        //    if (composite.BoolValue)
+        //    {
+        //        composite.StringValue += "Suffix";
+        //    }
+        //    return composite;
+        //}
+        public Correspon ObtenerCorrespon(string Cor_Nombre, int Cor_ID)
+        {
+            if (Cor_ID == 1)
+            {
+                return new Correspon();
+            }
+            else
+            {
+                return new Correspon() { ErrorCorrespon = "Erro al intetar Obtener la Informacion de la BD los Corresponsales" };
+
+            }
+        }
+
+
+        public Oficinas ObtenerOficinas(string Ofi_nombre, int Ofi_ID)
+        {
+            if (Ofi_ID == 1)
+            {
+                return new Oficinas();
+            }
+            else
+            {
+                return new Oficinas() { ErrorOficina = "Erro al intetar Obtener la Informacion de la BD los Corresponsales" };
+            }
+        }
+
+        public Oficinas ObtenerOficinas(string Ofi_nombre)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
+
+
+
